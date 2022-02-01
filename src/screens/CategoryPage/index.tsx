ce49b893521ac1097fcb9b77b404979889a36b5d
Your Name <you@example.com>
2022-02-01 14:04:24 +0200

    add featcherd product for empty products at category screen

--- src/screens/CategoryPage/index.tsx
+++ src/screens/CategoryPage/index.tsx
@@ -9,6 +9,8 @@ import RowComponent from "../../components/GlobalStyles/Row";
 import ProdectCard from "../../components/ProdectCard";
 import { getCategoryProducts } from "../../redux/actions/products.actions";
 import { AppState } from "../../redux/store";
+import { FeaturedProducts } from "../home/styled";
+import { SliderProduct } from "../../components/Slider";
 
 export default function CategoryPage() {
   const { keyword } = useParams();
@@ -24,7 +26,42 @@ export default function CategoryPage() {
   }, [dispatch, keyword]);
 
   const NotFoundProducts = (categoryProducts as IProduct[]).length === 0;
-  return (
+  return loading ? (
+    <Loading />
+  ) : NotFoundProducts ? (
+    <>
+      <Box p={"0 7% 70px"} sx={{ background: theme.palette.background.paper }}>
+        <Box p="50px 0">
+          <RowComponent
+            title={`${keyword}`}
+            widthDivider="200px"
+            alignItems="center"
+          />
+        </Box>
+        <Grid
+          spacing={5}
+          container
+          sx={{
+            justifyContent: "center",
+            height: loading || NotFoundProducts ? "65vh" : "100%",
+          }}
+        >
+          <div style={{ marginLeft: "40px" }}>
+            <img src="/static/no_products_found.png" alt="noproductsfound" />
+          </div>
+        </Grid>
+      </Box>
+      <FeaturedProducts style={{ background: 'white' }}>
+        <RowComponent
+          width="90%"
+          title={"Featured Products"}
+          widthDivider={"10%"}
+          alignItems="center"
+        />
+        <SliderProduct />
+      </FeaturedProducts>
+    </>
+  ) : (
     <Box p={"0 7% 70px"} sx={{ background: theme.palette.background.paper }}>
       <Box p="50px 0">
         <RowComponent
@@ -41,27 +78,19 @@ export default function CategoryPage() {
           height: loading || NotFoundProducts ? "65vh" : "100%",
         }}
       >
-        {loading ? (
-          <Loading />
-        ) : NotFoundProducts ? (
-          <div style={{ marginLeft: "40px" }}>
-            <img src="/static/no_products_found.png" alt="noproductsfound" />
-          </div>
-        ) : (
-          (categoryProducts as IProduct[]).map((product) => (
-            <Grid item xs={12} md={6} lg={4}>
-              <ProdectCard
-                key={product._id}
-                id={product._id as string}
-                img={product.images?.[0] || ""}
-                name={product.name}
-                price={product.price}
-                valueRating={product.rating}
-              />
-            </Grid>
-          ))
-        )}
+        {(categoryProducts as IProduct[]).map((product) => (
+          <Grid item xs={12} md={6} lg={4}>
+            <ProdectCard
+              key={product._id}
+              id={product._id as string}
+              img={product.images?.[0] || ""}
+              name={product.name}
+              price={product.price}
+              valueRating={product.rating}
+            />
+          </Grid>
+        ))}
       </Grid>
     </Box>
-  );
+  )
 }
